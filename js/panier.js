2f1865a09a42a5fb8fbc936f435bef88569fd62e
Aurore-Dimech <144670454+Aurore-Dimech@users.noreply.github.com>
2024-01-14 11:47:29 +0100

    📱 ADDED RESPONSIVE for all pages

--- js/panier.js
+++ js/panier.js
@@ -12,47 +12,57 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const fullTeam = JSON.parse(localStorage.getItem("teamList"));
     const fullCart = document.querySelector("#fullCart")
+    const releaseAllButton = document.querySelector("#releaseAll");
 
-    if (fullTeam.length === 0){
+    if (fullTeam === null || fullTeam.length === 0 ){
+        releaseAllButton.style.display = "none";
         const emptyCard = document.createElement('div')
         emptyCard.classList.add("emptyCard")
         emptyCard.innerHTML = "<h2> Vous n'avez pas encore de pokémon dans votre équipe !</h2> <img src='assets/cryingPokemon.webp' alt='Pokémon en train de pleurer'>"
         fullCart.appendChild(emptyCard);
+
+    } else {
+        
+        releaseAllButton.addEventListener("click", () => {
+            localStorage.removeItem("teamList");
+            location.reload();
+        })
+    
+        fullTeam.forEach((pokemon, i) => {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            .then(response => response.json())
+            .then((pokemon) => {
+                const indivCard = document.createElement('div')
+                indivCard.classList.add("indivCard")
+                indivCard.innerHTML = `
+                <img src="${pokemon.sprites.front_default}" alt="image de ${pokemon.name}" class="imgPokemon">
+                    <div class="pokemonCartInfo">
+                        <h2>${pokemon.name} (#${pokemon.id})</h2>
+                        <h3>${pokemon.types.map((type) => type.type.name).join(" & ")}</h3>
+                        <div class="statsRecap">
+                            ${pokemon.stats.map((stat) => `${stat.stat.name} : ${stat.base_stat}`).join("<p>")}
+                        </div>
+                    </div>
+                `
+                const releaseButton = document.createElement("button");
+                releaseButton.classList.add("removePokemon");
+                releaseButton.id = i;
+                releaseButton.textContent = "Relacher";
+                releaseButton.addEventListener("click", () => {
+                    const removePokemon = (index) => {
+                        const pokemonList = JSON.parse(localStorage.getItem("teamList")) || [];
+                        pokemonList.splice(index, 1);
+                        localStorage.setItem("teamList", JSON.stringify(pokemonList));
+                        location.reload();
+                    };
+                    removePokemon(i);
+                });
+                indivCard.appendChild(releaseButton);
+                releaseAllButton.before(indivCard);
+            })
+        })
     }
 
-    fullTeam.forEach((pokemon, i) => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        .then(response => response.json())
-        .then((pokemon) => {
-            const indivCard = document.createElement('div')
-            indivCard.classList.add("indivCard")
-            indivCard.innerHTML = `
-            <img src="${pokemon.sprites.front_default}" alt="image de ${pokemon.name}" class="imgPokemon">
-                <div class="pokemonCartInfo">
-                    <h2>${pokemon.name} (#${pokemon.id})</h2>
-                    <h3>${pokemon.types.map((type) => type.type.name).join(" & ")}</h3>
-                    <div class="statsRecap">
-                        ${pokemon.stats.map((stat) => `${stat.stat.name} : ${stat.base_stat}`).join("<p>")}
-                    </div>
-                </div>
-            `
-            const releaseButton = document.createElement("button");
-            releaseButton.classList.add("removePokemon");
-            releaseButton.id = i;
-            releaseButton.textContent = "Relacher";
-            releaseButton.addEventListener("click", () => {
-                const removePokemon = (index) => {
-                    const pokemonList = JSON.parse(localStorage.getItem("teamList")) || [];
-                    pokemonList.splice(index, 1);
-                    localStorage.setItem("teamList", JSON.stringify(pokemonList));
-                    location.reload();
-                };
-                removePokemon(i);
-            });
-            indivCard.appendChild(releaseButton);
-            fullCart.appendChild(indivCard);
-        })
-    })
 
 
 
